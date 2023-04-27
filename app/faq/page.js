91e3d0cd32f0a1f23b2e562a8659b4be09844bf0
In-Wess-Tation <skolekonto318@gmail.com>
2023-04-27 09:05:10 +0200

    new page

---
+++ app/faq/page.js
@@ -0,0 +1,24 @@
+
+
+const Faq = () => {
+    return ( 
+        <>
+        <h1>This is the FAQ page</h1>
+
+        <h2>Question one</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, molestias est eius assumenda libero tempore.</p>
+        
+        <h2>Question two</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, molestias est eius assumenda libero tempore.</p>
+
+        <h2>Question three</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, molestias est eius assumenda libero tempore.</p>
+        
+        <h2>Question four</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, molestias est eius assumenda libero tempore.</p>
+        
+        </>
+     );
+}
+ 
+export default Faq;
